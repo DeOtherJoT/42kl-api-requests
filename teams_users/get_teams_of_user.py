802a90e94f208c322942e60867b0518a8d26f845
Joshua Thor <jthor@42kl.edu.my>
2024-04-10 17:56:40 +0800

    feat: add scripts to get teams (project sessions) of a user
    In teams:
    The returned response is more detailed and is sorted based on the id in ascending order by default.
    
    In teams_users:
    The returned response has less detail and is sorted based on the id in descending order by default.

--- teams_users/get_teams_of_user.py
+++ teams_users/get_teams_of_user.py
@@ -18,7 +18,7 @@ if UID == None or SECRET == None:
 	raise (Exception("Env variables are not defined!"))
 
 SITE = "https://api.intra.42.fr"
-SCOPE = "public"
+SCOPE = "public projects"
 
 client = BackendApplicationClient(client_id=UID)
 oauth = OAuth2Session(client=client)
@@ -28,16 +28,15 @@ token = oauth.fetch_token(
 )
 
 # user_id = 172791
-user_id = "damdayan"
+user_id = "habdulla"
 
-# Load 30 "items" in one page.
 params = {
 	"page": {
-		"size": 100
+		"size": 20
     }
 }
 
-response = oauth.get(f"{SITE}/v2/users/{user_id}/teams", json=params)
+response = oauth.get(f"{SITE}/v2/users/{user_id}/teams_users", json=params)
 
 # Print response status and content
 if (response.status_code == 200):
@@ -46,26 +45,24 @@ else:
 	color = Fore.RED
 print(f"{color} {response.status_code}")
 
+# Below is just testing to see how to use the returned response.
 
-# Code below are for testing purposes
-
-# Load response into a JSON array and reverse it to arrange the project list from
-# most recent to least recent.
 data = json.loads(response.text)
-data = data[::-1]
+total = len(data)
 
-# From the reversed list, take the 5 most recent submitted and Momo-graded projects that are not exams
-# and get the project name (project_gitlab_path), id, and Momo's comment to check for Norme anomalies (teams_uploads)
-
+print("Items:\n")
 count = 0
+
+# From the response, extract the 5 most recent submitted projects (status is finished)
+# that are not exams (if exam then project_gitlab_path is None), and display the project
+# gitlab path and final mark.
 for item in data:
 	if count == 5:
 		break
-	if item['status'] != 'finished' or item['project_gitlab_path'] is None or len(item['teams_uploads']) == 0:
+	if item["team"]["status"] != "finished" or item['team']['project_gitlab_path'] is None:
 		continue
-	proj_name = str(item['project_gitlab_path']).split('/')[-1]
-	norme_count = len([1 for x in item['teams_uploads'][0]['comment'].split('|') if 'Norme' in x])
-	print(f"[{count}] : {proj_name} | {item['id']} | {norme_count}")
+	else:
+		print(f"[{count}] : {item['team']['project_gitlab_path']} with {item['team']['final_mark']}%")
 	count += 1
 
 json_obj = json.dumps(data, indent=4)
