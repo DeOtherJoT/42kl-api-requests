dae396ed5c97aff5b1fb260a414d8914088ba503
Josh Thor <jthor@42kl.edu.my>
2024-04-20 23:21:31 +0800

    chore: make output into json for readability

--- users/get_users.py
+++ users/get_users.py
@@ -2,6 +2,7 @@ import requests
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 from dotenv import load_dotenv
+import json
 import os
 
 load_dotenv()
@@ -26,9 +27,17 @@ token = oauth.fetch_token(
     token_url=f"{SITE}/oauth/token", client_id=UID, client_secret=SECRET, scope=SCOPE
 )
 
+# Determine who to stalk
+user_id = "zyeoh"
+
 # Make a request
-response = oauth.get(f"{SITE}/v2/users/wehuang")
+response = oauth.get(f"{SITE}/v2/users/{user_id}")
 
 # Print response status and content
 print(response.status_code)
-print(response.text)
+# print(response.text)
+
+data = json.loads(response.text)
+
+with open(f"user_{user_id}_data.json", "w") as outfile:
+    json.dump(data, outfile, indent=4)
